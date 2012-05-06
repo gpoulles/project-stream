e65b44d8e8bdcca92a1a1236297a3c48993f6701
Gilles Poulles <gpoulles@me.com>
2012-05-06 17:54:12 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectStream::Application.config.secret_token = 'de29165a107d451b75226ee62b739df2994c38e618c1b65e0785b408016c03aa6a6561f75ea94b6c98cc101973796ec72fbd7b610dc13c4aac12e3a7d822e5d3'
